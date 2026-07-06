cd1bb68c21c6b293052cace109c67e4cb5cf1f95
nodeanurag <conveytoanurag@gmail.com>
2026-07-06 22:04:58 +0530

    feat: integrate AuthRedirectHandler

--- swiftpoll-v1/src/app/page.tsx
+++ swiftpoll-v1/src/app/page.tsx
@@ -1,6 +1,9 @@
+import { AuthRedirectHandler } from "@/components/auth/auth-redirect-handler";
+
 export default function HomePage() {
   return (
     <div className="w-full">
+      <AuthRedirectHandler />
       {/* HomePage Shell */}
     </div>
   );
